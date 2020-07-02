d07d9cf0bbbac983a5f0adcd44435ea6afd61c0c
Marko Stijak <mstijak@gmail.com>
2020-07-02 10:48:23 +0200

    Prepare netlify publishing

---
+++ client/config/webpack.netlify.js
@@ -0,0 +1,21 @@
+const CopyWebpackPlugin = require ('copy-webpack-plugin'),
+  merge = require ('webpack-merge'),
+  prod = require ('./webpack.prod'),
+  path = require ('path');
+
+module.exports = merge (prod, {
+  plugins: [
+    new CopyWebpackPlugin ([
+      {
+        from: path.resolve (__dirname, './netlify.redirects'),
+        to: '_redirects',
+        toType: 'file',
+      },
+      {
+        from: path.resolve (__dirname, './netlify.headers'),
+        to: '_headers',
+        toType: 'file',
+      },
+    ]),
+  ],
+});

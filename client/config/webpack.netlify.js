b9c8b3669e5d9836b5beac1ad75c7ee51cea40c8
Marko Stijak <mstijak@gmail.com>
2020-07-06 23:05:44 +0200

    Fix the Netlify build

--- client/config/webpack.netlify.js
+++ client/config/webpack.netlify.js
@@ -1,21 +1,23 @@
-const CopyWebpackPlugin = require ('copy-webpack-plugin'),
-  merge = require ('webpack-merge'),
-  prod = require ('./webpack.prod'),
-  path = require ('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin'),
+   merge = require('webpack-merge'),
+   prod = require('./webpack.prod'),
+   path = require('path');
 
-module.exports = merge (prod, {
-  plugins: [
-    new CopyWebpackPlugin ([
-      {
-        from: path.resolve (__dirname, './netlify.redirects'),
-        to: '_redirects',
-        toType: 'file',
-      },
-      {
-        from: path.resolve (__dirname, './netlify.headers'),
-        to: '_headers',
-        toType: 'file',
-      },
-    ]),
-  ],
+module.exports = merge(prod, {
+   plugins: [
+      new CopyWebpackPlugin({
+         patterns: [
+            {
+               from: path.resolve(__dirname, './netlify.redirects'),
+               to: '_redirects',
+               toType: 'file',
+            },
+            {
+               from: path.resolve(__dirname, './netlify.headers'),
+               to: '_headers',
+               toType: 'file',
+            },
+         ],
+      }),
+   ],
 });
